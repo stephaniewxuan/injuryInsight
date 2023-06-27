6143983da00994f9214d4530b648ac10cf822908
stephaniewxuan <stephiewangg@gmail.com>
2023-06-27 14:19:14 -0700

    Initial Commit

---
+++ injuryInsight/injuryInsightApp.swift
@@ -0,0 +1,17 @@
+//
+//  injuryInsightApp.swift
+//  injuryInsight
+//
+//  Created by Stephanie Wang on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct injuryInsightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
